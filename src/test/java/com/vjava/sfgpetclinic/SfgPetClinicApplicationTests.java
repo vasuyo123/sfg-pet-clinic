27853c9969bc151c204beeeda92ae876fbaa34ce
Vasu Neelapala <vasu.yo@gmail.com>
2020-08-26 08:33:11 -0400

    Initial project add

---
+++ src/test/java/com/vjava/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vjava.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
