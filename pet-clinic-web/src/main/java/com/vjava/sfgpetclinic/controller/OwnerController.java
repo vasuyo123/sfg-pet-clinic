5dd3b1c1788109fd3ad19103fe789ccbf5b3ece8
Vasu Neelapala <vasu.yo@gmail.com>
2020-09-04 15:40:30 -0400

    Added Onwer index page and Owner controller. Closes #18

---
+++ pet-clinic-web/src/main/java/com/vjava/sfgpetclinic/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package com.vjava.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"/owners"})
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(){
+        return "owners/index";
+    }
+
+}
