60fd3b219c74eb4563bb2b51b0bdf5cfb7ea2720
Vasu Neelapala <vasu.yo@gmail.com>
2020-09-07 18:18:51 -0400

    Updated Owner with Pet relationship and Created Visit Entity. Closes #28

--- pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/model/Owner.java
@@ -1,4 +1,18 @@
 package com.vjava.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    private Set<Pet> pets;
+
+
 }
