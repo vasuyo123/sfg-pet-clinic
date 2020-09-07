cbd7f2487b42b8c88a889150b21f762d87dd6799
Vasu Neelapala <vasu.yo@gmail.com>
2020-09-07 19:35:37 -0400

    Enhanced Owners with Contact info and Pest. Closes #34

--- pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.vjava.sfgpetclinic.services.map;
 
 import com.vjava.sfgpetclinic.model.Owner;
+import com.vjava.sfgpetclinic.model.Pet;
 import com.vjava.sfgpetclinic.services.OwnerService;
+import com.vjava.sfgpetclinic.services.PetService;
+import com.vjava.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -26,7 +37,26 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+        if(owner != null){
+            if(owner.getPets() != null){
+                owner.getPets().forEach(pet -> {
+                    if(pet.getPetType() != null){
+                        if(pet.getPetType().getId() == null){
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }else{
+                        throw new RuntimeException("Pet Type can't be NULL");
+                    }
+                    if(pet.getPetType() == null){
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(owner);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/vjava/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/vjava/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.vjava.sfgpetclinic.bootstrap;
 
 import com.vjava.sfgpetclinic.model.Owner;
+import com.vjava.sfgpetclinic.model.Pet;
 import com.vjava.sfgpetclinic.model.PetType;
 import com.vjava.sfgpetclinic.model.Vet;
 import com.vjava.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.vjava.sfgpetclinic.services.PetTypeService;
 import com.vjava.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -31,21 +34,54 @@ public class DataLoader implements CommandLineRunner {
         dog.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+        PetType tiger = new PetType();
+        dog.setName("Tiger");
+        PetType savedTigerPetType = petTypeService.save(tiger);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Street");
+        owner1.setCity("His City");
+        owner1.setTelephone("234-678-5433");
 
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setName("Mikes Dog");
+        mikesPet.setBirthDate( LocalDate.now());
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glen");
+        owner2.setAddress("123 Ave");
+        owner2.setCity("Their City");
+        owner2.setTelephone("634-678-4433");
+
+        Pet fionasPet = new Pet();
+        fionasPet.setPetType(savedCatPetType);
+        fionasPet.setOwner(owner2);
+        fionasPet.setName("Fionas Cat");
+        fionasPet.setBirthDate( LocalDate.now());
+        owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("Vasu");
         owner3.setLastName("Neelapala");
+        owner3.setAddress("123 Dr");
+        owner3.setCity("Happy City");
+        owner3.setTelephone("534-322-4356");
+
+        Pet vasusTiger = new Pet();
+        vasusTiger.setPetType(savedTigerPetType);
+        vasusTiger.setOwner(owner2);
+        vasusTiger.setName("Fionas Cat");
+        vasusTiger.setBirthDate( LocalDate.now());
+        owner3.getPets().add(vasusTiger);
 
         ownerService.save(owner3);
 
