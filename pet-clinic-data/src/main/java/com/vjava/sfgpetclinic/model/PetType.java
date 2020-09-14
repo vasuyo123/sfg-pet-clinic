55ae1722c368deb144275a09f530b7c541a90828
Vasu Neelapala <vasu.yo@gmail.com>
2020-09-14 16:41:25 -0400

    Error on Display Owner fixed Closes #63

--- pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/model/PetType.java
@@ -13,10 +13,15 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+//@ToString
 public class PetType extends BaseEntity{
 
     @Column(name = "name")
     private String name;
 
+    @Override
+    public String toString(){
+        return name;
+    }
 
 }
--- pet-clinic-web/src/main/java/com/vjava/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vjava/sfgpetclinic/controller/OwnerController.java
@@ -1,18 +1,25 @@
 package com.vjava.sfgpetclinic.controller;
 
+import com.vjava.sfgpetclinic.model.Owner;
 import com.vjava.sfgpetclinic.services.OwnerService;
+import com.vjava.sfgpetclinic.services.VisitService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
 
     private final OwnerService ownerService;
+    private final VisitService visitService;
 
-    public OwnerController(OwnerService ownerService) {
+    public OwnerController(OwnerService ownerService, VisitService visitService) {
         this.ownerService = ownerService;
+        this.visitService = visitService;
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
@@ -24,6 +31,14 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notimpmeneted";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 
 }
