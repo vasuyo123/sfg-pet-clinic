5299ef8a7edf1b79c7d20cf62b2f71df9bb0b4db
Vasu Neelapala <vasu.yo@gmail.com>
2020-09-17 08:39:34 -0400

    Added VisitController for adding and updating Visit. Closes #61

--- pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vjava/sfgpetclinic/model/Pet.java
@@ -40,6 +40,8 @@ public class Pet extends BaseEntity{
         this.owner = owner;
         this.birthDate = birthDate;
         this.name = name;
-        this.visits = visits;
+        if(visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
     }
 }
